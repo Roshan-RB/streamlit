156b39eb84c93af483e63b347ef0848b4b599a3b
Niharika2208 <niharikajois@gmail.com>
2024-03-24 12:47:13 +0100

    PDF uploader

--- Image_Cropping.py
+++ Image_Cropping.py
@@ -1,9 +1,10 @@
 import streamlit as st
+from streamlit_cropperjs import st_cropperjs
 import fitz  # PyMuPDF
 import tempfile
 import os
 
-st.title("PDF Viewer")
+st.title("PDF Cropper")
 
 pdf_file = st.file_uploader("Upload a PDF file", type="pdf")
 page_number = st.number_input("Enter the page number:", min_value=1, format="%d", value=1)
@@ -20,7 +21,18 @@ if pdf_file is not None:
     else:
         page = pdf_document.load_page(page_number - 1)
         image_bytes = page.get_pixmap().tobytes()
-        st.image(image_bytes)
+
+        # Display the PDF page
+        st.image(image_bytes, use_column_width=True, format="PNG", caption=f"Page {page_number}")
+
+        # Use cropper to select and crop a part of the image
+        cropped_image = st_cropperjs(image_bytes, aspect_ratio=(4, 3))
+
+        # Download the cropped image
+        if cropped_image is not None:
+            st.write("Cropped Image:")
+            st.image(cropped_image, use_column_width=True, format="PNG")
+            st.download_button("Download Cropped Image", cropped_image, file_name="cropped_image.png", mime="image/png")
 
     # Clean up the temporary file
     os.unlink(tmp_file_path)
