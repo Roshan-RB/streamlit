2c8941505603b3eef8e80b990b5eaacafeb0ecd9
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2024-03-24 12:31:23 +0100

    Update Image_Cropping.py

--- Image_Cropping.py
+++ Image_Cropping.py
@@ -1,15 +1,20 @@
 import streamlit as st
+import fitz  # PyMuPDF
 
-from streamlit_cropperjs import st_cropperjs
+st.title("PDF Viewer")
 
-st.title("Streamlit-Cropperjs")
-st.write("Integrating cropperjs by fengyuanchen with streamlit")
-pic = st.file_uploader("Upload a picture", key="uploaded_pic", type="pdf")
-if pic:
-    pic = pic.read()
-    cropped_pic = st_cropperjs(pic=pic, btn_text="Detect!", key="foo")
-    if cropped_pic:
-        st.image(cropped_pic, output_format="PNG")
-        st.download_button(
-            "Download", cropped_pic, file_name="output.png", mime="image/png"
-        )
+pdf_file = st.file_uploader("Upload a PDF file", type="pdf")
+page_number = st.number_input("Enter the page number:", min_value=1, format="%d", value=1)
+
+if pdf_file is not None:
+    pdf_document = fitz.open(stream=pdf_file, filetype="pdf")
+
+    if page_number > len(pdf_document):
+        st.error(f"Invalid page number. Maximum page number is {len(pdf_document)}.")
+    else:
+        page = pdf_document.load_page(page_number - 1)
+        page_text = page.get_text()
+        st.write(f"Page {page_number} of {len(pdf_document)}:")
+        st.write(page_text)
+else:
+    st.write("Upload a PDF file using the file uploader above.")
