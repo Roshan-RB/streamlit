905551362ede7277ac29d513a1affeee8d14d3f6
Niharika2208 <55751831+Niharika2208@users.noreply.github.com>
2024-03-24 12:14:07 +0100

    Update Image_Cropping.py

--- Image_Cropping.py
+++ Image_Cropping.py
@@ -4,7 +4,7 @@ from streamlit_cropperjs import st_cropperjs
 
 st.title("Streamlit-Cropperjs")
 st.write("Integrating cropperjs by fengyuanchen with streamlit")
-pic = st.file_uploader("Upload a picture", key="uploaded_pic")
+pic = st.file_uploader("Upload a picture", key="uploaded_pic", type="pdf")
 if pic:
     pic = pic.read()
     cropped_pic = st_cropperjs(pic=pic, btn_text="Detect!", key="foo")
